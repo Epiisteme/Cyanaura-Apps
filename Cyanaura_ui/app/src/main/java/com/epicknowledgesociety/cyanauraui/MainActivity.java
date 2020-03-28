00733d5c3c60aa3890b46d54a78fe52e55ef4db7
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-28 19:33:24 +0530

    changes in UI

--- Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/MainActivity.java
+++ Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/MainActivity.java
@@ -2,13 +2,27 @@ package com.epicknowledgesociety.cyanauraui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox isInfected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isInfected=(CheckBox)findViewById(R.id.isinfected);
+        isInfected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,InfectedPersonnel.class);
+                isInfected.setChecked(false);
+                startActivity(intent);
+            }
+        });
+
     }
 }
