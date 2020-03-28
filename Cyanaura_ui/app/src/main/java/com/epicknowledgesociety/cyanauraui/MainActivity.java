7e33d3901846b74ff4535731b8b1ca2132a1890f
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-28 20:02:44 +0530

    updated spinner for gender

--- Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/MainActivity.java
+++ Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/MainActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox isInfected;
+    Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         isInfected=(CheckBox)findViewById(R.id.isinfected);
+        spinner1=(Spinner)findViewById(R.id.ugender);
         isInfected.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,7 +29,20 @@ public class MainActivity extends AppCompatActivity {
                 isInfected.setChecked(false);
                 startActivity(intent);
             }
+
+
         });
+        List<String> categories = new ArrayList<String>();
+        categories.add("Male");
+        categories.add("Female");
+        categories.add("Other");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner1.setAdapter(dataAdapter);
 
     }
 }
