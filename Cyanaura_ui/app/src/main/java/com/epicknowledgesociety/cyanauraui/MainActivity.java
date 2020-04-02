2c01526bb9f556d09f58dc4ce8a6a8bc09d947e2
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-04-02 23:54:26 +0530

    notification bar

--- Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/MainActivity.java
+++ Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/MainActivity.java
@@ -2,8 +2,12 @@ package com.epicknowledgesociety.cyanauraui;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -25,76 +29,82 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.transform.Result;
+
 public class MainActivity extends AppCompatActivity {
     CheckBox isInfected;
     Spinner spinner1;
-    Button symptomdate,confirmdate,statusdate,submit1;
-    String symptomdatestr,confirmdatestr,statusdatestr,infsourcestr,inforiginstr,unamestr,udistrictstr,umuncipalitystr,uagestr,unationalitystr,uresidentstr,ucaddressstr,upaddressstr,ugenderstr;
+    Button symptomdate, confirmdate, statusdate, submit1;
+    String symptomdatestr, confirmdatestr, statusdatestr, infsourcestr, inforiginstr, unamestr, udistrictstr, umuncipalitystr, uagestr, unationalitystr, uresidentstr, ucaddressstr, upaddressstr, ugenderstr;
     int isinfectedint;
 
-    EditText uname,udistrict,umuncipality,uage,unationality,uresident,ucaddress,upaddress,infsource,inforigin;
+    EditText uname, udistrict, umuncipality, uage, unationality, uresident, ucaddress, upaddress, infsource, inforigin;
     androidx.constraintlayout.widget.ConstraintLayout l1;
     SQLiteDatabase sqLiteDatabase;
-    int y=2020;
+    int y = 2020;
     boolean CheckEditTextEmpty;
     String sqlitequery;
-
+    NotificationCompat.Builder notification;
+    PendingIntent pIntent;
+    NotificationManager manager;
+    Intent resultIntent;
+    TaskStackBuilder stackBuilder;
+    public static final String NOTIFICATION_CHANNEL_ID ="Cyanaura";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        isInfected=(CheckBox)findViewById(R.id.isinfected);
+        isInfected = (CheckBox) findViewById(R.id.isinfected);
 
-        infsource=(EditText)findViewById(R.id.uinfectionsource);
-        inforigin=(EditText)findViewById(R.id.uinfectionorigin);
-        uname=(EditText)findViewById(R.id.uname);
-        udistrict=(EditText)findViewById(R.id.udistrict);
-        umuncipality=(EditText)findViewById(R.id.umuncipality);
-        uage=(EditText)findViewById(R.id.uage);
-        unationality=(EditText)findViewById(R.id.unationality);
-        uresident=(EditText)findViewById(R.id.uresidentstate);
-        ucaddress=(EditText)findViewById(R.id.ucurrentaddress);
-        upaddress=(EditText)findViewById(R.id.upermanentaddress);
+        infsource = (EditText) findViewById(R.id.uinfectionsource);
+        inforigin = (EditText) findViewById(R.id.uinfectionorigin);
+        uname = (EditText) findViewById(R.id.uname);
+        udistrict = (EditText) findViewById(R.id.udistrict);
+        umuncipality = (EditText) findViewById(R.id.umuncipality);
+        uage = (EditText) findViewById(R.id.uage);
+        unationality = (EditText) findViewById(R.id.unationality);
+        uresident = (EditText) findViewById(R.id.uresidentstate);
+        ucaddress = (EditText) findViewById(R.id.ucurrentaddress);
+        upaddress = (EditText) findViewById(R.id.upermanentaddress);
 
 
-        unamestr=uname.getText().toString();
-        udistrictstr=udistrict.getText().toString();
-        umuncipalitystr=umuncipality.getText().toString();
-        uagestr=uage.getText().toString();
-        unationalitystr=unationality.getText().toString();
-        uresidentstr=uresident.getText().toString();
-        ucaddressstr=ucaddress.getText().toString();
-        upaddressstr=upaddress.getText().toString();
-        infsourcestr=infsource.getText().toString();
-        inforiginstr=inforigin.getText().toString();
+        unamestr = uname.getText().toString();
+        udistrictstr = udistrict.getText().toString();
+        umuncipalitystr = umuncipality.getText().toString();
+        uagestr = uage.getText().toString();
+        unationalitystr = unationality.getText().toString();
+        uresidentstr = uresident.getText().toString();
+        ucaddressstr = ucaddress.getText().toString();
+        upaddressstr = upaddress.getText().toString();
+        infsourcestr = infsource.getText().toString();
+        inforiginstr = inforigin.getText().toString();
 
-        symptomdate=(Button)findViewById(R.id.usymptomdate);
-        confirmdate=(Button)findViewById(R.id.uconfirmdate);
-        statusdate=(Button)findViewById(R.id.ustatusdate);
+        symptomdate = (Button) findViewById(R.id.usymptomdate);
+        confirmdate = (Button) findViewById(R.id.uconfirmdate);
+        statusdate = (Button) findViewById(R.id.ustatusdate);
 
-        submit1=(Button)findViewById(R.id.submit1);
-        l1=( androidx.constraintlayout.widget.ConstraintLayout)findViewById(R.id.cl1);
+        submit1 = (Button) findViewById(R.id.submit1);
+        l1 = (androidx.constraintlayout.widget.ConstraintLayout) findViewById(R.id.cl1);
 
-        spinner1=(Spinner)findViewById(R.id.ugender);
+        spinner1 = (Spinner) findViewById(R.id.ugender);
 
 
         isInfected.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(l1.getVisibility()==View.GONE) {
+                if (l1.getVisibility() == View.GONE) {
                     l1.setVisibility(View.VISIBLE);
-                    isinfectedint=1;
+                    isinfectedint = 1;
 
-                }
-                else{
-                   l1.setVisibility(View.GONE);
-                   isinfectedint=0;
+                } else {
+                    l1.setVisibility(View.GONE);
+                    isinfectedint = 0;
                 }
 
-   // infsourcestr=infsource.getText().toString();
-    //inforiginstr=inforigin.getText().toString();
+                // infsourcestr=infsource.getText().toString();
+                //inforiginstr=inforigin.getText().toString();
 
 
             }
@@ -105,15 +115,14 @@ public class MainActivity extends AppCompatActivity {
         symptomdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog d1=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog d1 = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        symptomdatestr=dayOfMonth+"/"+month+"/"+year;
-
+                        symptomdatestr = dayOfMonth + "/" + month + "/" + year;
 
 
                     }
-                },y,0,0);
+                }, y, 0, 0);
                 d1.show();
 
             }
@@ -122,13 +131,13 @@ public class MainActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                DatePickerDialog d2=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog d2 = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        confirmdatestr=dayOfMonth+"/"+month+"/"+year;
+                        confirmdatestr = dayOfMonth + "/" + month + "/" + year;
 
                     }
-                },y,0,0);
+                }, y, 0, 0);
                 d2.show();
 
             }
@@ -136,18 +145,16 @@ public class MainActivity extends AppCompatActivity {
         statusdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog d3=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog d3 = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        statusdatestr=dayOfMonth+"/"+month+"/"+year;
+                        statusdatestr = dayOfMonth + "/" + month + "/" + year;
                     }
-                },y,0,0);
+                }, y, 0, 0);
                 d3.show();
 
             }
         });
-
-
 
 
         List<String> categories = new ArrayList<String>();
@@ -164,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                ugenderstr=String.valueOf(spinner1.getSelectedItem());
+                ugenderstr = String.valueOf(spinner1.getSelectedItem());
             }
 
             @Override
@@ -177,67 +184,98 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 DBCreate();
                 SubmitData2SQLiteDB();
+                startNotification();
             }
+
         });
 
 
+
     }
-    public void DBCreate(){
+
+    public void DBCreate() {
 
         sqLiteDatabase = openOrCreateDatabase("DetailDataBase", Context.MODE_PRIVATE, null);
 
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS demoTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Name VARCHAR, District VARCHAR, Muncipality VARCHAR,Age INT,Gender VARCHAR,Nationality VARCHAR,Resident_State VARCHAR,Current_Address VARCHAR,Permanent_Address VARCHAR,Is_Infected VARCHAR,Symptoms_Date VARCHAR,Symptom_Confirm_Date VARCHAR,Status_Date VARCHAR,Infection_Origin VARCHAR,Infection_Source VARCHAR);");
     }
 
-    public void CheckEditTextIsEmptyOrNot( ){
+    public void CheckEditTextIsEmptyOrNot() {
 
-        if(TextUtils.isEmpty(unamestr) || TextUtils.isEmpty(udistrictstr) || TextUtils.isEmpty(umuncipalitystr) ||TextUtils.isEmpty(uagestr) || TextUtils.isEmpty(unationalitystr) || TextUtils.isEmpty(uresidentstr) || TextUtils.isEmpty(ucaddressstr) || TextUtils.isEmpty(upaddressstr)) {
+        if (TextUtils.isEmpty(unamestr) || TextUtils.isEmpty(udistrictstr) || TextUtils.isEmpty(umuncipalitystr) || TextUtils.isEmpty(uagestr) || TextUtils.isEmpty(unationalitystr) || TextUtils.isEmpty(uresidentstr) || TextUtils.isEmpty(ucaddressstr) || TextUtils.isEmpty(upaddressstr)) {
 
 
-            CheckEditTextEmpty = false ;
+            CheckEditTextEmpty = false;
 
-        }
-        else {
-            CheckEditTextEmpty = true ;
+        } else {
+            CheckEditTextEmpty = true;
         }
     }
-    public void ClearEditTextAfterDoneTask(){
+
+    public void ClearEditTextAfterDoneTask() {
 
         uname.getText().clear();
         udistrict.getText().clear();
         umuncipality.getText().clear();
         uage.getText().clear();
         unationality.getText().clear();
-        
+
         uresident.getText().clear();
         ucaddress.getText().clear();
         upaddress.getText().clear();
 
     }
 
-    public void SubmitData2SQLiteDB(){
+    public void SubmitData2SQLiteDB() {
         CheckEditTextIsEmptyOrNot();
 
-        if(CheckEditTextEmpty == false)
-        {
+        if (CheckEditTextEmpty == false) {
 
-            sqlitequery = "INSERT INTO demoTable (Name,District,Muncipality,Age,Gender,Nationality,Resident_State,Current_Address,Permanent_Address,Is_Infected,Symptoms_Date,Symptom_Confirm_Date,Status_Date,Infection_Origin,Infection_Source) VALUES('"+unamestr+"', '"+udistrictstr+"', '"+umuncipalitystr+"','"+uagestr+"','"+ugenderstr+"','"+unationalitystr+"','"+uresidentstr+"','"+ucaddressstr+"','"+upaddressstr+"','"+isinfectedint+"','"+symptomdatestr+"','"+confirmdatestr+"','"+statusdatestr+"','"+inforiginstr+"','"+infsourcestr+"');";
+            sqlitequery = "INSERT INTO demoTable (Name,District,Muncipality,Age,Gender,Nationality,Resident_State,Current_Address,Permanent_Address,Is_Infected,Symptoms_Date,Symptom_Confirm_Date,Status_Date,Infection_Origin,Infection_Source) VALUES('" + unamestr + "', '" + udistrictstr + "', '" + umuncipalitystr + "','" + uagestr + "','" + ugenderstr + "','" + unationalitystr + "','" + uresidentstr + "','" + ucaddressstr + "','" + upaddressstr + "','" + isinfectedint + "','" + symptomdatestr + "','" + confirmdatestr + "','" + statusdatestr + "','" + inforiginstr + "','" + infsourcestr + "');";
 
             sqLiteDatabase.execSQL(sqlitequery);
 
-            Toast.makeText(MainActivity.this,"Data Submitted Successfully", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Data Submitted Successfully", Toast.LENGTH_LONG).show();
 
             ClearEditTextAfterDoneTask();
-            Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//            startActivity(intent);
 
-        }
-        else {
+        } else {
 
-            Toast.makeText(MainActivity.this,"Please Fill All the Fields", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Please Fill All the Fields", Toast.LENGTH_LONG).show();
         }
+
+
     }
+    protected void startNotification() {
+        // TODO Auto-generated method stub
+        //Creating Notification Builder
+        notification = new NotificationCompat.Builder(MainActivity.this, NOTIFICATION_CHANNEL_ID);
+        //Title for Notification
+        notification.setContentTitle("Learn2Crack Updates");
+        //Message in the Notification
+        notification.setContentText("New Post on Android Notification.");
+        //Alert shown when Notification is received
+        notification.setTicker("New Message Alert!");
+        //Icon to be set on Notification
+        notification.setSmallIcon(R.drawable.ic_launcher_background);
+        //Creating new Stack Builder
+                stackBuilder = TaskStackBuilder.create(MainActivity.this);
+                stackBuilder.addParentStack(Result.class);
+                //Intent which is opened when notification is clicked
+                resultIntent = new Intent(MainActivity.this, Result.class);
+                stackBuilder.addNextIntent(resultIntent);
+                pIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                notification.setContentIntent(pIntent);
+                manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, notification.build());
+
+    }
+
+
 }
+
 
 
 
---
+++ Cyanaura_ui/app/src/main/java/com/epicknowledgesociety/cyanauraui/Result.java
@@ -0,0 +1,14 @@
+package com.epicknowledgesociety.cyanauraui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Result extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+
+}
