26285ebe404a18b28f86cead6d0c3be3a4ff1359
Akash Vijayan <akash.vijayan99@gmail.com>
2020-08-12 17:44:48 +0530

    location permissions added'

--- Cyanaura_app_bluetooth/app/src/main/java/com/epicknowledgesociety/cyanaura_java/FoundBTDevices.java
+++ Cyanaura_app_bluetooth/app/src/main/java/com/epicknowledgesociety/cyanaura_java/FoundBTDevices.java
@@ -7,9 +7,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import android.content.IntentSender;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 
 public class FoundBTDevices extends ListActivity
@@ -21,61 +37,61 @@ public class FoundBTDevices extends ListActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
 
         displayListOfFoundDevices();
     }
 
-private void displayListOfFoundDevices()
-{
-    arrayOfFoundBTDevices = new ArrayList<BluetoothObject>();
-
-    // start looking for bluetooth devices
-    mBluetoothAdapter.startDiscovery();
-
-    // Discover new devices
-    // Create a BroadcastReceiver for ACTION_FOUND
-    final BroadcastReceiver mReceiver = new BroadcastReceiver()
+    private void displayListOfFoundDevices()
     {
-        @Override
-        public void onReceive(Context context, Intent intent)
+        arrayOfFoundBTDevices = new ArrayList<BluetoothObject>();
+
+        // start looking for bluetooth devices
+        mBluetoothAdapter.startDiscovery();
+
+        // Discover new devices
+        // Create a BroadcastReceiver for ACTION_FOUND
+        final BroadcastReceiver mReceiver = new BroadcastReceiver()
         {
-            String action = intent.getAction();
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action))
+            @Override
+            public void onReceive(Context context, Intent intent)
             {
-                // Get the bluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action))
+                {
+                    // Get the bluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
-                // Get the "RSSI" to get the signal strength as integer,
-                // but should be displayed in "dBm" units
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                    // Get the "RSSI" to get the signal strength as integer,
+                    // but should be displayed in "dBm" units
+                    int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
 
-                // Create the device object and add it to the arrayList of devices
-                BluetoothObject bluetoothObject = new BluetoothObject();
-                bluetoothObject.setBluetooth_name(device.getName());
-                bluetoothObject.setBluetooth_address(device.getAddress());
-                bluetoothObject.setBluetooth_state(device.getBondState());
-                bluetoothObject.setBluetooth_type(device.getType());    // requires API 18 or higher
-                bluetoothObject.setBluetooth_uuids(device.getUuids());
-                bluetoothObject.setBluetooth_rssi(rssi);
+                    // Create the device object and add it to the arrayList of devices
+                    BluetoothObject bluetoothObject = new BluetoothObject();
+                    bluetoothObject.setBluetooth_name(device.getName());
+                    bluetoothObject.setBluetooth_address(device.getAddress());
+                    bluetoothObject.setBluetooth_state(device.getBondState());
+                    bluetoothObject.setBluetooth_type(device.getType());    // requires API 18 or higher
+                    bluetoothObject.setBluetooth_uuids(device.getUuids());
+                    bluetoothObject.setBluetooth_rssi(rssi);
 
-                arrayOfFoundBTDevices.add(bluetoothObject);
+                    arrayOfFoundBTDevices.add(bluetoothObject);
 
-                // 1. Pass context and data to the custom adapter
-                FoundBTDevicesAdapter adapter = new FoundBTDevicesAdapter(getApplicationContext(), arrayOfFoundBTDevices);
+                    // 1. Pass context and data to the custom adapter
+                    FoundBTDevicesAdapter adapter = new FoundBTDevicesAdapter(getApplicationContext(), arrayOfFoundBTDevices);
 
-                // 2. setListAdapter
-                setListAdapter(adapter);
+                    // 2. setListAdapter
+                    setListAdapter(adapter);
+                }
             }
-        }
-    };
-    // Register the BroadcastReceiver
-    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-    registerReceiver(mReceiver, filter);
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
 
-}
+    }
 
     @Override
     protected void onPause()
@@ -85,5 +101,5 @@ private void displayListOfFoundDevices()
         super.onPause();
 
     }
-}
 
+}
--- Cyanaura_app_bluetooth/app/src/main/java/com/epicknowledgesociety/cyanaura_java/MainActivity.java
+++ Cyanaura_app_bluetooth/app/src/main/java/com/epicknowledgesociety/cyanaura_java/MainActivity.java
@@ -1,6 +1,8 @@
 package com.epicknowledgesociety.cyanaura_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -8,6 +10,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,14 +20,27 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
 import java.util.ArrayList;
 import java.util.Set;
+
+import static android.content.ContentValues.TAG;
 
 
 public class MainActivity extends AppCompatActivity {
     private String LOG_TAG; // Just for logging purposes. Could be anything. Set to app_name
     private int REQUEST_ENABLE_BT = 99; // Any positive integer should work.
     private BluetoothAdapter mBluetoothAdapter;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     private Button button_enableBT;
     private Button button_displayPairedBT;
@@ -34,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LOG_TAG = getResources().getString(R.string.app_name);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions( this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -64,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         button_scanBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                scanForBluetoothDevices();
+                checkLocationEnabled(getApplicationContext());
             }
         });
     }//end onCreate
@@ -130,5 +154,56 @@ public class MainActivity extends AppCompatActivity {
         // Start this on a new activity without passing any data to it
         Intent intent = new Intent(this, FoundBTDevices.class);
         startActivity(intent);
+    }
+    public  void checkLocationEnabled(Context context) {
+        LocationManager lm = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            if(! lm.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                        .addApi(LocationServices.API).build();
+                googleApiClient.connect();
+
+                LocationRequest locationRequest = LocationRequest.create();
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                locationRequest.setInterval(10000);
+                locationRequest.setFastestInterval(10000 / 2);
+
+                LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+                builder.setAlwaysShow(true);
+
+                PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+                result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                    @Override
+                    public void onResult(LocationSettingsResult result) {
+                        final Status status = result.getStatus();
+                        switch (status.getStatusCode()) {
+                            case LocationSettingsStatusCodes.SUCCESS:
+                                Log.i(TAG, "All location settings are satisfied.");
+                                break;
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the result
+                                    // in onActivityResult().
+                                    status.startResolutionForResult(MainActivity.this , REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException e) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                                break;
+                        }
+                    }
+                });
+            }
+            else{
+                scanForBluetoothDevices();
+            }
+        }catch(Exception ex) {}
+
+
     }
 }
